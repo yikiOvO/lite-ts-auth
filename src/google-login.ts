447bd5e476a7e64f0eb4ccc8de8e9586b1208aea
linyuxian <2731904064@qq.com>
2023-03-17 10:28:36 +0800

    feat:google登录

--- src/google-login.ts
+++ src/google-login.ts
@@ -14,7 +14,6 @@ export class GoogleLogin implements ILogin {
         }
         const data: any = {};
         data.callback = 'window.loginCb';
-        //jsb.reflection.callStaticMethod方法可能会cc层java交互方案修改而改变,后续优化需要封装
         try {
             const resp = await GoogleLogin.jsb.reflection.callStaticMethod('com/ily/core/jsb/JSBridgeManager', 'googleLogin', '(Ljava/lang/String;)V', data);
             return resp;
