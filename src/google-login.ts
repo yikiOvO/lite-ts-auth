bd9842bc07e73f6c0eea5cc15bd0cf2180457ba6
linyuxian <2731904064@qq.com>
2023-03-28 09:35:56 +0800

    feat:代码优化

--- src/google-login.ts
+++ src/google-login.ts
@@ -1,15 +1,19 @@
-import { NativeLoginBase } from "./native-login-base";
+import { NativeLoginBase } from './native-login-base';
 
 type GoogleLoginResponse = {
     idToken: string,
 }
 
 export class GoogleLogin extends NativeLoginBase {
-    protected getLoginBody(loginData: GoogleLoginResponse): any {
-        return { google: { idToken: loginData.idToken } };
+    protected getLoginBody(loginData: GoogleLoginResponse) {
+        return {
+            google: {
+                idToken: loginData.idToken
+            }
+        };
     }
 
-    protected getLoginPlatform(): string {
+    protected getLoginPlatform() {
         return 'Google';
     }
 }
