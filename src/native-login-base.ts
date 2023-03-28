3a51c1cb0ae645e5c67c522ba079435431112256
linyuxian <2731904064@qq.com>
2023-03-28 09:33:35 +0800

    feat:代码优化

--- src/native-login-base.ts
+++ src/native-login-base.ts
@@ -1,21 +1,17 @@
-import { AjaxRpc } from "lite-ts-ajax";
-import { Header, RpcBase } from "lite-ts-rpc";
+import { AjaxRpc } from 'lite-ts-ajax';
+import { Header, RpcBase } from 'lite-ts-rpc';
 
-import { BuildLoginOption } from "./login-factory-base";
-import { ILogin } from "./i-login";
-import { LoginResponse } from "./login-response";
+import { BuildLoginOption } from './login-factory-base';
+import { ILogin } from './i-login';
+import { LoginResponse } from './login-response';
 
 export abstract class NativeLoginBase implements ILogin {
     public static jsb: any;
 
     public constructor(
-        protected m_Opt: BuildLoginOption,
-        protected m_Rpc: RpcBase,
+        protected opt: BuildLoginOption,
+        protected rpc: RpcBase,
     ) { }
-
-    protected abstract getLoginBody(loginData: any): any;
-
-    protected abstract getLoginPlatform(): string;
 
     public async login() {
         globalThis['loginCb'] = async <T extends LoginResponse>(e, r) => {
@@ -24,10 +20,10 @@ export abstract class NativeLoginBase implements ILogin {
                 return new Error(e);
 
             const body = this.getLoginBody(r);
-            const resp = await this.m_Rpc.callWithoutThrow<T>({
+            const resp = await this.rpc.callWithoutThrow<T>({
                 route: '/account/login',
                 body: {
-                    ...this.m_Opt,
+                    ...this.opt,
                     ...body
                 }
             });
@@ -36,17 +32,21 @@ export abstract class NativeLoginBase implements ILogin {
             return resp.data;
         }
 
-        try {
-            if (!NativeLoginBase.jsb)
-                throw new Error(`${this.constructor.name}.jsb未绑定`);
-            let data: any = {};
-            data.callback = 'globalThis.loginCb';
-            data.loginPlatform = this.getLoginPlatform();
-            data = JSON.stringify(data);
-            const resp = await NativeLoginBase.jsb.reflection.callStaticMethod('JSBridgeManager', "emitEventLogin:", data);
-            return resp;
-        } catch (error) {
-            throw new Error(error);
-        }
+        if (!NativeLoginBase.jsb)
+            throw new Error(`${this.constructor.name}.jsb未绑定`);
+
+        return await NativeLoginBase.jsb.reflection.callStaticMethod(
+            'JSBridgeManager',
+            'emitEventLogin:',
+            JSON.stringify({
+                callback: 'globalThis.loginCb',
+                loginPlatform: this.getLoginPlatform()
+            })
+        );
     }
+
+    protected abstract getLoginBody(loginData: any): any;
+
+    protected abstract getLoginPlatform(): string;
+
 }
